0720e0a6b07425a02a9439dded305cce1dd757ec
Andrea Rojas <andrear.2810@gmail.com>
2021-04-06 13:00:32 -0500

    Finished the fourth tutorial

--- index.js
+++ index.js
@@ -15,4 +15,8 @@ var io = socket(server);
 
 io.on('connection', (socket) => {
     console.log('Made socket connection', socket.id);
+    socket.on('chat', function(data){
+        //Send to the different clients
+        io.sockets.emit('chat', data);
+    });
 });
